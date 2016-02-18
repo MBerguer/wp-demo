24960e8cd25e8e90b671926354b2636553eb5e11
raul <raul@mgcoders.com>
2016-02-18 16:05:11 -0300

    limpio

--- wordpress/themes/delirium/functions/theme/config.php
+++
@@ -1,8 +0,0 @@
-<?php return array(
-/**
- * Theme configuration goes there
- */
-    'name'    => 'Delirium',   					// theme name
-    'styled'  => false,              			// true if current theme supports styling
-    'scripts' => array("dropdown", "tabs")		// javascripts theme uses
-);
